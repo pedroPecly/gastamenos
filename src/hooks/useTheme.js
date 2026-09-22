e27f27f81f554362bf902fdf7da6347273f83f78
Pedro henrique Pecly Gomes <135339009+pedroPecly@users.noreply.github.com>
2026-09-22 12:25:03 -0300

    ajustando safe area

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -12,10 +12,14 @@ export function useTheme() {
 
   useEffect(() => {
     const root = window.document.documentElement;
+    const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+
     if (theme === 'dark') {
       root.classList.add('dark');
+      if (metaThemeColor) metaThemeColor.setAttribute('content', '#030712');
     } else {
       root.classList.remove('dark');
+      if (metaThemeColor) metaThemeColor.setAttribute('content', '#F2F2F7');
     }
     localStorage.setItem('@financeApp:theme', theme);
   }, [theme]);
